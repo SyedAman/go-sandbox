4540a8439abff1c9ede7fe3ad87f91e9cc1b89a2
Syed Aman <sna274@nyu.edu>
2017-11-25 16:45:23 -0600

    move the main function to the bottom because it makes more sense syntactically

--- src/main.go
+++ src/main.go
@@ -2,30 +2,39 @@ package main
 
 import "fmt"
 
-func main() {
-	newUser := createNewUser("Blargo")
-	fmt.Println("Name:", newUser.firstName)
-
-	var outsideTemperature = getWeather(50)
-	fmt.Println("Weather in Fahrenheit:", outsideTemperature.temperatureF)
-	fmt.Println("Weather in Celsius:", outsideTemperature.temperatureC)
-}
-
 // User ... the user playing the game.
 type User struct {
 	firstName string
 }
 
+// Weather defines F and C temperatures
 type Weather struct {
 	temperatureF float32
 	temperatureC float32
 }
 
-// gets the F and C temperatures
-func getWeather(fahrenheit float32) Weather {
+// GetWeather gets the F and C temperatures
+func GetWeather(fahrenheit float32) Weather {
 	return Weather{fahrenheit, (fahrenheit - 32) * .5556}
 }
 
-func createNewUser(firstName string) User {
+// CreateNewUser creates a new user
+func CreateNewUser(firstName string) User {
 	return User{firstName}
+}
+
+// DisplayWeather prints out the weather in Fahrenheit and Celsius
+func DisplayWeather(weather Weather) {
+  fmt.Println("Weather in Fahrenheit:", weather.temperatureF)
+  fmt.Println("Weather in Celsius:", weather.temperatureC)
+}
+
+func main() {
+  // create user
+	newUser := CreateNewUser("Blargo")
+	fmt.Println("Name:", newUser.firstName)
+
+  // get the weather today
+	var outsideTemperature = GetWeather(50)
+  DisplayWeather(outsideTemperature)
 }
